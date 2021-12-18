1b364f1173289c7ffbb77a81ae1091198db18790
yanpilogova <anpilogova1986@gmail.com>
2021-12-18 11:13:57 +0700

    added files

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+      int currentBalance = 2_000_000_000;
+      int transfer = 500_000_000;
+      int total = currentBalance + transfer;
+        System.out.println(total);
+    }
+}
