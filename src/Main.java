41156b072352193ea68e5b0b614ac95224419a57
yanpilogova <anpilogova1986@gmail.com>
2021-12-20 14:30:42 +0700

    report.md added

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
       int currentBalance = 2_000_000_000;
       int transfer = 500_000_000;
-      int total = currentBalance + transfer;
+      int total = transfer + currentBalance;
         System.out.println(total);
     }
 }
